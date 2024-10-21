fdc5920272aeab3d6401716ff6a7059786bac3f7
MiniKlar <omontloris@gmail.com>
2024-10-21 13:22:08 +0200

    modified:   ft_memcpy.c     from 42 School

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,32 @@
 /*   By: lomont <lomont@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 11:59:49 by lomont            #+#    #+#             */
-/*   Updated: 2024/10/21 12:06:28 by lomont           ###   ########.fr       */
+/*   Updated: 2024/10/21 13:22:00 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void *memcpy(void *dest, const void *src, size_t n)
+void *ft_memcpy(void *dest, const void *src, size_t n)
 {
+    size_t i;
 
+    i = 0;
+
+    while (i < n)
+    {
+        *(unsigned char *)dest = *(unsigned char *)src;
+        dest++;
+        src++;
+        i++;
+    }
+    return (dest);
+}
+int main(void)
+{
+    char    dest[100] = "SALUT00000000";
+    char    src[100] = "111111";
+    
+    ft_memcpy(dest, src, 10);
+    printf("%s\n", dest);
 }
