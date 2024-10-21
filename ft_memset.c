5bba48ffcc87bec4db5e8f70ebdfdf24ca662127
MiniKlar <omontloris@gmail.com>
2024-10-21 11:40:25 +0200

    modified:   ft_memset.c from 42 school

--- ft_memset.c
+++ ft_memset.c
@@ -3,36 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miniklar <miniklar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lomont <lomont@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 00:04:35 by miniklar          #+#    #+#             */
-/*   Updated: 2024/10/21 01:06:54 by miniklar         ###   ########.fr       */
+/*   Updated: 2024/10/21 11:36:37 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void *memset(void *s, int c, size_t n)
+void *ft_memset(void *s, int c, size_t n)
 {
     size_t  i;
-    unsigned char    str;
-
+    
     i = 0;
-    str = *((unsigned char *)s);
     if (n == 0)
         return (NULL);
     while (i < n)
     {
-        str[i] = (unsigned char) c;
+        *(unsigned char *)s = (unsigned char) c;
+        s++;
         i++;
     }
     return (s);
 }
 int main(void)
 {
-    char    str[] = "Salut";
-    char *p;
-
-    p = memset(str, '0', 2);
-    printf("%s\n", p);
+    char    buffer[100];
+    
+    ft_memset(buffer, '0', 100);
+    printf("%s\n", buffer);
 }
